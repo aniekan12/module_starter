f2cc66bee18ad23bb31e3a398424dda16bece4e0
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2024-07-03 10:54:52 +0100

    create identity token annotation

--- lib/common/io/annotations/api_annotations.dart
+++ lib/common/io/annotations/api_annotations.dart
@@ -1,8 +1,14 @@
 import 'package:retrofit/retrofit.dart';
 
 const requiresAuthorizationKey = 'requires-authentication';
+const requiresIndentityTokenKey = 'requires-identity-token';
 
 /// Specifies if an Endpoint should add the Authorization Token
 class Authorized extends Extra {
   const Authorized() : super(const {requiresAuthorizationKey: true});
+}
+
+/// Specifies if an Endpoint should add the Identity Token
+class IdentityToken extends Extra {
+  const IdentityToken() : super(const {requiresIndentityTokenKey: true});
 }
