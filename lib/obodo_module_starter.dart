f784e0034b14a817013f6cb202d0c8f101a55c30
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2024-12-09 09:00:14 +0100

    expose device info provider api

--- lib/obodo_module_starter.dart
+++ lib/obodo_module_starter.dart
@@ -2,3 +2,5 @@ library obodo_module_starter;
 
 export 'package:obodo_module_starter/common/io/api_client.dart';
 export 'package:obodo_module_starter/common/io/annotations/api_annotations.dart';
+export 'package:obodo_module_starter/domain/providers/device_info_provider.dart';
+export 'package:obodo_module_starter/common/di/app_module.dart';
